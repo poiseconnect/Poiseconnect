f012e505907c4fe7996ffbd7f4835061e5c095e9
poiseconnect <Hallo@mypoise.de>
2025-10-30 07:21:51 +0000

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Poise Connect",
+  description: "Poise Klientenanfrage System",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
