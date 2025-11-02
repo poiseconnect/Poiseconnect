90549d898dcbb2df76afea3d8bde69db6a7718c8
poiseconnect <Hallo@mypoise.de>
2025-11-02 15:45:31 +0000

    Update route.js

--- app/api/submit/route.js
+++ app/api/submit/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import Resend from "resend";
+import { Resend } from "resend";
 import { createClient } from "@supabase/supabase-js";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
