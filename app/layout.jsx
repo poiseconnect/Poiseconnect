40888649b78a7267c39b7854ca21232b9e43f3da
poiseconnect <Hallo@mypoise.de>
2025-11-03 23:06:49 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,16 @@
 export const metadata = {
   title: "Poise Connect",
-  description: "Poise Klientenanfrage System",
+  description: "Poise Anfrageformular",
 };
+
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Playfair+Display:wght@400;600&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
