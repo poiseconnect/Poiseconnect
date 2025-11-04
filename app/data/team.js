dbb8e7f2c443cd0e18208f6751a6c2681fadbe95
poiseconnect <Hallo@mypoise.de>
2025-11-05 00:54:59 +0100

    Update team.js

--- app/data/team.js
+++ app/data/team.js
@@ -1,1 +1,141 @@
+export const team = [
+  {
+    name: "Linda",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Ann",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Anna",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Anja",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Babette",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Carolin",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Caroline",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Elena",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Franziska",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Gerhard",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Gesine",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Isabella",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Jenny",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Judith",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Julius",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Kristin",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Kristin-Sofie",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Livia",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Magdalena",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Marisa",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Marleen",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Sophie",
+    tags: [],
+    image: "",
+    available: true
+  },
+  {
+    name: "Yanina",
+    tags: [],
+    image: "",
+    available: true
+  }
+];
 
