75a52229af2b058477b190c6dee4677ac6bf9555
poiseconnect <Hallo@mypoise.de>
2025-11-04 13:38:43 +0100

    Create teamData.js

---
+++ app/teamData.js
@@ -0,0 +1,17 @@
+export const teamData = [
+  {
+    name: "Ann",
+    role: "Heilpraktikerin für Psychotherapie, Hypnose, Master Coach",
+    image: "https://mypoise.de/wp-content/uploads/2023/01/DSC_0007-copy-2-1300x1300.jpg",
+    status: "frei", // oder "warteliste"
+    tags: ["Selbstwert", "Grenzen", "Stress"], // Platzhalter
+  },
+  {
+    name: "Anna",
+    role: "Coach & Trainerin, Psychoonkologin & Heilpraktikerin für Psychotherapie",
+    image: "https://mypoise.de/wp-content/uploads/2023/06/Anna-Moser-scaled-e1739306168495-1300x1300.jpg",
+    status: "frei",
+    tags: ["Übergänge", "Beziehungen", "Identität"],
+  },
+  // … alle anderen Teammitglieder hier einfügen (du hast URLs schon geliefert)
+];
