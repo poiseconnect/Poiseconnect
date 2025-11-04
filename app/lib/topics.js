5ac3ae3e6f36e207b93ce3ddf950a35f896473d2
poiseconnect <Hallo@mypoise.de>
2025-11-04 23:25:41 +0100

    Create topics.js

---
+++ app/lib/topics.js
@@ -0,0 +1,54 @@
+export const TOPIC_KEYWORDS = {
+  selbstwert: [
+    "unsicher", "minderwertig", "zweifel", "zweifeln", "nicht gut genug",
+    "scham", "selbstkritik", "perfektionismus", "vergleichen", "abwertung"
+  ],
+
+  angst: [
+    "angst", "panik", "panikattacke", "sorge", "überwältigt", "überforderung",
+    "unruhe", "innere unruhe", "kontrollverlust", "dauerdruck"
+  ],
+
+  stress: [
+    "stress", "gestresst", "überlastet", "erschöpft", "ausgelaugt",
+    "keine kraft", "zu viel", "keine pause", "funktionieren müssen"
+  ],
+
+  beziehung: [
+    "beziehung", "partnerschaft", "partner", "trennung", "nähe",
+    "bindungsangst", "eifersucht"
+  ],
+
+  emotionen: [
+    "gefühle", "emotional", "wut", "zu viel gefühl", "zu wenig gefühl"
+  ],
+
+  trauma: [
+    "trauma", "flashback", "trigger", "ohnmacht", "erstarrung"
+  ],
+
+  depression: [
+    "traurig", "leer", "hoffnungslos", "nichts fühlen", "antriebslos",
+    "niedergeschlagen"
+  ],
+
+  trauer: [
+    "verlust", "vermissen", "abschied", "trauern"
+  ],
+
+  identität: [
+    "wer bin ich", "orientierung", "sinnsuche", "lebenskrise"
+  ],
+
+  psychosomatik: [
+    "körper reagiert", "körperliche symptome", "brustdruck", "magen"
+  ],
+
+  frauenleben: [
+    "schwangerschaft", "mutterschaft", "mutterrolle", "zyklus", "körperbild"
+  ],
+
+  arbeit: [
+    "arbeit", "job", "karriere", "teamkonflikt", "arbeitsplatz"
+  ]
+};
