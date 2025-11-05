41221dcbc7e8baf462f1656ae28772360d7cb4ef
poiseconnect <Hallo@mypoise.de>
2025-11-05 01:23:06 +0100

    Update team.js

--- app/data/team.js
+++ app/data/team.js
@@ -1,140 +1,134 @@
 export const team = [
   {
-    name: "Linda",
-    tags: [],
-    image: "",
-    available: true
-  },
-  {
     name: "Ann",
-    tags: [],
-    image: "",
+    image: "https://mypoise.de/wp-content/uploads/2023/01/DSC_0007-copy-2-1300x1300.jpg",
+    tags: ["beziehung", "selbstwert", "burnout", "panik"],
     available: true
   },
   {
     name: "Anna",
+    image: "https://mypoise.de/wp-content/uploads/2023/06/Anna-Moser-scaled-e1739306168495-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Anja",
+    image: "https://mypoise.de/wp-content/uploads/2022/11/Vorstellungsfoto-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Babette",
+    image: "https://mypoise.de/wp-content/uploads/2022/11/IMG_7891-scaled-e1669712492385-700x700.jpeg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Carolin",
+    image: "https://mypoise.de/wp-content/uploads/2023/09/163_Portrait-Juni2023__pp-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Caroline",
+    image: "https://mypoise.de/wp-content/uploads/2023/12/IMG_7279-700x700.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Elena",
+    image: "https://mypoise.de/wp-content/uploads/2025/02/Image-scaled-e1740223110391.jpeg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Franziska",
+    image: "https://mypoise.de/wp-content/uploads/2022/12/1654680310653.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Gerhard",
+    image: "https://mypoise.de/wp-content/uploads/2025/04/Frontfoto-1024x1024.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Gesine",
+    image: "https://mypoise.de/wp-content/uploads/2025/08/Bildschirmfoto-2025-08-04-um-14.55.22.png",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Isabella",
+    image: "https://mypoise.de/wp-content/uploads/2024/08/IsabellaKusztrits%C2%A9DanielaGruber-13.jpeg-600x426.png",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Jenny",
+    image: "https://mypoise.de/wp-content/uploads/2022/11/Screen-Shot-2022-11-22-at-09.59.06-700x700.png",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Judith",
+    image: "https://mypoise.de/wp-content/uploads/2022/11/Judith_Gross-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Julius",
+    image: "https://mypoise.de/wp-content/uploads/2023/09/Poise_bild-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Kristin",
+    image: "https://mypoise.de/wp-content/uploads/2024/01/202411_CG_LiliKristin_276-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Kristin-Sofie",
+    image: "https://mypoise.de/wp-content/uploads/2024/07/DSCF6979-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Livia",
+    image: "https://mypoise.de/wp-content/uploads/2025/10/Foto-11-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Magdalena",
+    image: "https://mypoise.de/wp-content/uploads/2023/01/Magdalena-e1672744279356-700x700.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Marisa",
+    image: "https://mypoise.de/wp-content/uploads/2022/12/IMG_9908-e1669989498337-700x700.jpeg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Marleen",
+    image: "https://mypoise.de/wp-content/uploads/2022/11/Foto-Homepage-1300x1300.jpg",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Sophie",
+    image: "https://mypoise.de/wp-content/uploads/2022/11/Foto-sophie-crop-1-e1668080175188.png",
     tags: [],
-    image: "",
     available: true
   },
   {
     name: "Yanina",
+    image: "https://mypoise.de/wp-content/uploads/2024/11/IMG_7943-scaled-e1732804924432-1022x1024.jpg",
     tags: [],
-    image: "",
     available: true
   }
 ];
