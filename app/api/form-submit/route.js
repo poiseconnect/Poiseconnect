3e0c341765dc3d0e785c6486cb36a36d7332b602
poiseconnect <Hallo@mypoise.de>
2025-11-24 10:14:39 +0100

    Update route.js

--- app/api/form-submit/route.js
+++ app/api/form-submit/route.js
@@ -36,7 +36,7 @@ export async function POST(request) {
     // ✅ Links für Auswahl durch Teammitglied
     const base = "https://poiseconnect.vercel.app/api/therapist-response";
 
-const confirmLink = `${base}?action=confirm&client=${encodeURIComponent(email)}&name=${encodeURIComponent(vorname)}`;
+const confirmLink = `${base}?action=confirm&client=${encodeURIComponent(email)}&name=${encodeURIComponent(vorname)}&time=${encodeURIComponent(data.terminISO)}`;
 
 const rebookSameLink = `${base}?action=rebook_same&client=${encodeURIComponent(email)}&name=${encodeURIComponent(
   vorname
