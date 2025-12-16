812cf1aea75418752870178ac159308510372801
poiseconnect <Hallo@mypoise.de>
2025-12-17 00:02:30 +0100

    Update route.js

--- app/api/add-sessions-batch/route.js
+++ app/api/add-sessions-batch/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { supabase } from "@/app/lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 export const dynamic = "force-dynamic";
 
