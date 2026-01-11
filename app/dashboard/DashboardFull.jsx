94a7d0f29f4c1870f2adf64982e521332f90281d
poiseconnect <Hallo@mypoise.de>
2026-01-11 01:01:16 +0100

    Update DashboardFull.jsx

--- app/dashboard/DashboardFull.jsx
+++ app/dashboard/DashboardFull.jsx
@@ -2,7 +2,8 @@
 
 import { useEffect, useMemo, useState } from "react";
 import { supabase } from "../lib/supabase";
-import { teamData } from "@/lib/teamData";
+import { teamData } from "../lib/teamData";
+
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 
