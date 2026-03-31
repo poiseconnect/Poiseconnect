7e7f0967155bfbf1c57a515e3739cf1a381d08bd
poiseconnect <Hallo@mypoise.de>
2026-03-31 12:23:11 +0200

    Fix import path for server library in route.js

--- app/api/update-meeting-link-override/route.js
+++ app/api/update-meeting-link-override/route.js
@@ -1,6 +1,6 @@
 export const dynamic = "force-dynamic";
 
-import { json, supabaseAdmin } from "../../_lib/server";
+import { json, supabaseAdmin } from "../_lib/server";
 
 export async function POST(req) {
   try {
